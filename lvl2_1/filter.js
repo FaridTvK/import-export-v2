44f34c88005cbad2730db047b709683f3d106f9c
FaridTvK <farid@super-code.de>
2023-07-04 15:55:04 +0200

    done

---
+++ lvl2_1/filter.js
@@ -0,0 +1,9 @@
+import { data } from "./cities.js";
+
+export const filterPopulation1 = () => {
+    return data.filter((city) => city.population > 100000);
+};
+
+export const filterPopulation2 = () => {
+    return data.filter((city) => city.population < 100000);
+};
---
+++ lvl2_1/index.js
@@ -0,0 +1,4 @@
+import { filterPopulation1, filterPopulation2 } from "./filter.js";
+
+console.log(filterPopulation1());
+console.log(filterPopulation2());
